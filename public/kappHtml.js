0edd674853f1a87cffe4eb64f3bfab5ed0b9f37e
Michele Mendel <michelemendel@gmail.com>
2018-12-31 10:55:45 +0100

    Changes to product view. Changed colors.

--- public/kappHtml.js
+++ public/kappHtml.js
@@ -68,52 +68,55 @@ function createProductNode(product) {
 }
 
 const display_cols = {
-    "category": "Category",
-    "sub_category": "Sub Category",
-    "producer": "Producer",
-    "product": "Product",
-    "kosher_type": "Kosher Type",
-    "kosher_stamp": "Kosher Stamp",
-    "product_type": "Product Type",
-    "comment": "Comment",
+    category: "Category",
+    sub_category: "Sub Category",
+    producer: "Producer",
+    product: "Product",
+    kosher_type: "Kosher Type",
+    kosher_stamp: "Kosher Stamp",
+    product_type: "Product Type",
+    comment: "Comment",
     // 
-    "cat_sub_cat": "Category/SubCat"
+    cat_sub_cat: "Category/Subcat",
+    kosher_type_stamp: "Kosher",
 };
 
 const kosher_type_lookup = {
     "-": "-",
     "p": "parve",
     "m": "melk",
-    "mprod": "ikke melk, produsert i melk-utstyr",
+    "mprod": "Ikke melk, men produsert i melke-utstyr",
     "ik": "ikke kosher",
 };
 
 function createProductItemNodes(product) {
     const prodList = {
-        "cat_sub_cat": product["category"] + "/" + product["sub_category"],
-        "producer": product["producer"],
-        "product": product["product"],
-        "kosher_type": kosher_type_lookup[product["kosher_type"]],
-        "kosher_stamp": product["kosher_stamp"],
-        "product_type": product["product_type"],
-        "comment": product["comment"],
+        category: product["sub_category"] !== "-" ? "-" : product["category"],
+        cat_sub_cat: product["sub_category"] !== "-" ? product["category"] + "/" + product["sub_category"] : "-",
+        producer: product["producer"],
+        product_type: product["product_type"],
+        product: product["product"],
+        kosher_type_stamp: kosher_type_lookup[product["kosher_type"]] + (product["kosher_stamp"] !== "-" ? " (" + product["kosher_stamp"] + ")" : ""),
+        comment: product["comment"],
     };
 
-    return Object.keys(prodList).map((key) => {
-        contentKeyNode = document.createElement("div");
-        contentKeyNode.classList.add("contentKey");
-        contentKeyNode.appendChild(createContentNode(display_cols[key]))
+    return Object.keys(prodList)
+        .filter((key) => prodList[key].trim() !== "-")
+        .map((key) => {
+            contentKeyNode = document.createElement("div");
+            contentKeyNode.classList.add("contentKey");
+            contentKeyNode.appendChild(createContentNode(display_cols[key]))
 
-        contentValueNode = document.createElement("div");
-        contentValueNode.classList.add("contentValue");
-        contentValueNode.appendChild(createContentNode(prodList[key]))
+            contentValueNode = document.createElement("div");
+            contentValueNode.classList.add("contentValue");
+            contentValueNode.appendChild(createContentNode(prodList[key]))
 
-        productItemNode = document.createElement("div");
-        productItemNode.classList.add("productItem");
-        productItemNode.appendChild(contentKeyNode);
-        productItemNode.appendChild(contentValueNode);
-        return productItemNode;
-    });
+            productItemNode = document.createElement("div");
+            productItemNode.classList.add("productItem");
+            productItemNode.appendChild(contentKeyNode);
+            productItemNode.appendChild(contentValueNode);
+            return productItemNode;
+        });
 }
 
 function createContentNode(content) {
